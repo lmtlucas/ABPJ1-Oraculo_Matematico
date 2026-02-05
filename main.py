f0fa69f814aca12dea2082cb00251664f8d3e976
Lucas M. T. <90627091+lmtlucas@users.noreply.github.com>
2026-02-05 19:46:55 -0300

    Atualização para deixar claro as saídas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(f'***BEM-VINDO AO PLANEJADOR DE META FINANCEIRA*** \n - Primeiro você digitará os valores da sua meta financeira e da quantidade de meses para atingi-lá.\n - Em seguida será solicitado a digitar sua renda e as despesas mensais. \n - Para finalizar digite 0.')
+print(f'***BEM-VINDO AO PLANEJADOR DE META FINANCEIRA*** \n - Primeiro você digitará os valores da sua meta financeira e da quantidade de meses para atingi-lá.\n - Em seguida será solicitado a digitar sua renda e as suas despesas mensais. \n - Para finalizar digite 0.')
 
 meta = float(input('Digite a sua meta: '))
 meses = int(input('Digite quantos meses para atingir a meta: '))
@@ -10,11 +10,11 @@ while despesas[-1] != 0:
     despesas.append(float(input('Digite a valor de uma despesa mensal fixa (ou 0 para finalizar): ')))
 
 total_despesas = sum(despesas)
-sobras = renda - total_despesas
-acumulados = sobras * meses
+economia = renda - total_despesas
+acumulados = economia * meses
 
 if acumulados >= meta:
-    print(f'Você atingiu sua meta!!!\nEm cada mês sua sobra foi de R${sobras:.2f}, somando as sobras ao longo dos {meses} meses, você acumulo o total de: R${acumulados:.2f}')
+    print(f'Você atingiu sua meta!!!\nEm cada mês você economizou R${economia:.2f}, somando essas economias ao longo dos {meses} meses, você acumulo o total de: R${acumulados:.2f}\n - Total de despesas mensais: R${total_despesas}\n - Total economizado por mês: R${economia} ')
 else:  
-    print(f'Você não atingiu a meta\nEm cada mês sua sobra foi de R${sobras:.2f}, somando as sobras ao longo dos {meses} meses, você acumulo o total de: R${acumulados:.2f}')
+    print(f'Você não atingiu a meta\nEm cada mês você economizou R${economia:.2f}, somando essas economias  ao longo dos {meses} meses, você acumulo o total de: R${acumulados:.2f}\n - Total de despesas mensais: R${total_despesas}\n - Total economizado por mês: R${economia} ')
 
